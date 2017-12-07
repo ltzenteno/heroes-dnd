8b6fb029b96d229b7ce59004311120f520b18482
Luis Zenteno <ltzenteno@gmail.com>
2017-12-06 18:30:06 -0600

    organizes divs inside blueprint

--- src/client/app/hero/components/BluePrint.js
+++ src/client/app/hero/components/BluePrint.js
@@ -16,6 +16,26 @@ class BluePrint extends Component{
             <div style={{width:styles.torsoTarget.width, height:styles.torsoTarget.height}}/>
           </DropSpot>
         </div>
+        <div style={styles.rightArmTarget}>
+          <DropSpot>
+            <div style={{width:styles.rightArmTarget.width, height:styles.rightArmTarget.height}}/>
+          </DropSpot>
+        </div>
+        <div style={styles.leftArmTarget}>
+          <DropSpot>
+            <div style={{width:styles.leftArmTarget.width, height:styles.leftArmTarget.height}}/>
+          </DropSpot>
+        </div>
+        <div style={styles.rightLegTarget}>
+          <DropSpot>
+            <div style={{width:styles.rightLegTarget.width, height:styles.rightLegTarget.height}}/>
+          </DropSpot>
+        </div>
+        <div style={styles.leftLegTarget}>
+          <DropSpot>
+            <div style={{width:styles.leftLegTarget.width, height:styles.leftLegTarget.height}}/>
+          </DropSpot>
+        </div>
       </div>
     );
   }
@@ -41,6 +61,34 @@ const styles = {
     height:270,
     marginTop:43,
     marginLeft:298
+  },
+  rightArmTarget:{
+    backgroundColor:'rgba(255,0,0,0.2)',
+    width:100,
+    height:200,
+    marginTop:-150,
+    marginLeft:178
+  },
+  leftArmTarget:{
+    backgroundColor:'rgba(255,0,0,0.2)',
+    width:100,
+    height:200,
+    marginTop:-205,
+    marginLeft:418
+  },
+  rightLegTarget:{
+    backgroundColor:'rgba(255,0,0,0.2)',
+    width:100,
+    height:200,
+    marginTop:30,
+    marginLeft:228
+  },
+  leftLegTarget:{
+    backgroundColor:'rgba(255,0,0,0.2)',
+    width:100,
+    height:200,
+    marginTop:-200,
+    marginLeft:358
   }
 };
 
